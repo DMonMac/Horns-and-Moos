27d7f229cf681823320dad45c7dd21a52bdc6821
DMonMac <dmmacaso@gmail.com>
2018-01-18 11:29:06 +0800

    Number console now generates numbers according to required number of digits (see console during inspect)

--- src/Components/NumberConsole.js
+++ src/Components/NumberConsole.js
@@ -4,34 +4,53 @@ class NumberConsole extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      number: 0,
+      number: '',
+      digits: 4
+    };
+    this.newNumber = this.newNumber.bind(this);
+  }
+
+  componentDidMount() {
+    let numberGen = Math.floor(Math.random()*Math.floor(Math.pow(10, this.state.digits)));
+    let length = this.state.digits;
+    let numberToGuess = '' + numberGen;
+    while (numberToGuess.length < length) {
+      numberToGuess = '0' + numberToGuess
+      console.log(numberToGuess)
     }
+    this.setState({number: numberToGuess});
   }
 
-  generateNumber() {
-    console.log("Created secret number")
-    console.log(Math.random())
+  newNumber() {
+    let numberGen = Math.floor(Math.random()*Math.floor(Math.pow(10, this.state.digits)));
+    let length = this.state.digits;
+    let numberToGuess = '' + numberGen;
+    while (numberToGuess.length < length) {
+      numberToGuess = '0' + numberToGuess
+    }
+    this.setState({number: numberToGuess});
   }
 
-  showResult(e) {
-    e.preventDefault();
-    console.log('Show Bulls and Cows');
+  showResult() {
+    console.log("Showing result of guessed number");
   }
 
   render() {
-    console.log(this.props.number)
+    console.log("Created new number: " + this.state.number)
     return (
       <div>
-        <h3>Number Game Component:</h3>
-        <button onClick={this.generateNumber}>New Game</button>
-        <form>
-          <input
-            type="number"
-            min="0"
-            max="9999"
-            placeholder="Input number here"
-          /><button onClick={this.showResult}>Try</button><br/>
-        </form>
+        <h3>Numerical Bulls and Cows:</h3>
+        <button onClick={this.newNumber}>New Game</button>
+          <div>
+            <input
+              //value ='0000'
+              ref={(input) => {this.numberInput = input; }}
+              type="number"
+              min='0'
+              max={Math.pow(10, this.state.digits)-1}
+              placeholder="Input number here"
+            /><button onClick={this.showResult}>Try</button><br/>
+          </div>
       </div>
     );
   }
