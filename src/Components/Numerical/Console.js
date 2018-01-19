1ef96aa2d0127bbe0ff30963c984759b00e078d1
DMonMac <dmmacaso@gmail.com>
2018-01-19 20:48:46 +0800

    Input now clears after an input; Cleaned up console messages; Changed Bulls and Cows and Horns and Moos respectively

--- src/Components/Numerical/Console.js
+++ src/Components/Numerical/Console.js
@@ -14,9 +14,8 @@ class Console extends Component {
     this.newGame = this.newGame.bind(this);
     this.inputGuess = this.inputGuess.bind(this);
     this.resultCount = this.resultCount.bind(this);
-    this.countBulls = this.countBulls.bind(this);
   }
-
+  //Genereate number when loaded
   componentDidMount() {
     let numberGen = Math.floor(Math.random()*Math.floor(Math.pow(10, this.state.digits)));
     let length = this.state.digits;
@@ -26,18 +25,22 @@ class Console extends Component {
     }
     this.setState({number: numberToGuess}, () => {
       console.log("Created new number: " + this.state.number)
+      console.log("Initializing...")
       console.log("Input: " + this.state.input)
+      console.log("Records:")
       console.log(this.state.records)
     });
   }
 
   newGame() {
+    // Generate number
     let numberGen = Math.floor(Math.random()*Math.floor(Math.pow(10, this.state.digits)));
     let length = this.state.digits;
     let numberToGuess = '' + numberGen;
     while (numberToGuess.length < length) {
       numberToGuess = '0' + numberToGuess
     }
+    // Reinitialize states
     this.setState({
       number: numberToGuess,
       input: '',
@@ -49,80 +52,85 @@ class Console extends Component {
              }
     );
   }
-
+  // Required so that it's possible to input numbers
   inputGuess(event) {
     this.setState({input: event.target.value})
   }
-
-  countBulls() {
-    // Required values
-    let numDigits = this.state.number.split('')
-    let inputDigits = this.state.input.split('')
-    let records = this.state.records
-
-    // Initialize count
-    let bulls = 0
-    let bullIndeces = []
-    let cows = 0
-
-    //Solve for Bulls:
-    console.log('Solve for Bulls:')
-    for(var i = numDigits.length -1; i >=0; i--){
-      console.log("Index: " + i)
-      console.log("Number Digit: " + numDigits[i])
-      let inputDigit = inputDigits[i]
-      console.log("Guess Digit: " + inputDigit)
-      if (inputDigit === numDigits[i]) {
-        bulls++;
-        bullIndeces = bullIndeces.concat(i)
-      }
-    }
-
-    //Solve for Cows
-    console.log('Solve for Cows:')
-    console.log(bullIndeces)
-    bullIndeces.forEach(index => {
-      numDigits.splice(index,1)
-      inputDigits.splice(index,1)
-      console.log(numDigits)
-      console.log(inputDigits)
-    })
-    console.log(numDigits)
-    console.log(inputDigits)
-
-    inputDigits.forEach(digit => {
-      let numIndexToRemove = numDigits.indexOf(digit)
-      console.log("numindex: "+numIndexToRemove)
-      if (numIndexToRemove !== -1) {
-        numDigits.splice(numIndexToRemove,1)
-        console.log(numDigits)
-        console.log(inputDigits)
-        cows++;
-      }
-    })
-
-    console.log(records)
-
-    this.setState({
-      records: records.concat(
-        [
-          {
-            guess: this.state.input,
-            bulls: bulls,
-            bullIndeces: bullIndeces,
-            cows: cows
-          }
-        ]
-      )
-    }, () => {
-      console.log(this.state.records)
-    })
-  }
-
+  // Count Horns and Moos on click
   resultCount(event) {
     event.preventDefault();
     if (this.state.input.length === this.state.digits) {
-      this.countBulls()
+      // Required values
+      let numDigits = this.state.number.split('')
+      let inputDigits = this.state.input.split('')
+      let records = this.state.records
+
+      // Initialize count
+      let horns = 0
+      let hornIndeces = []
+      let moos = 0
+      console.log('Guess: ' + this.state.input)
+      console.log('Results for ' + this.state.input + ":")
+      
+      //Solve for Horns:
+      console.log('Solve for Horns:')
+      // Compare number and guess by index
+      for(var i = numDigits.length -1; i >=0; i--){
+        console.log("Index: " + i)
+        console.log("Number Digit: " + numDigits[i])
+        let inputDigit = inputDigits[i]
+        console.log("Guess Digit: " + inputDigit)
+        if (inputDigit === numDigits[i]) {
+          horns++;
+          hornIndeces = hornIndeces.concat(i)
+        }
+      }
+      console.log("Horns: " + horns)
+      console.log("Horn Indeces:")
+      console.log(hornIndeces)
+
+      //Solve for Cows
+      console.log('Solve for Moos:')
+      console.log('Removing these indeces from Number Digits Array:')
+      console.log(hornIndeces)
+      hornIndeces.forEach(index => {
+        numDigits.splice(index,1)
+        inputDigits.splice(index,1)
+      })
+      console.log('Remaining digits to check:')
+      console.log(numDigits)
+      console.log('Check with these digits:')
+      console.log(inputDigits)
+
+      inputDigits.forEach(digit => {
+        let numIndexToRemove = numDigits.indexOf(digit)
+        if (numIndexToRemove !== -1) {
+          numDigits.splice(numIndexToRemove,1)
+          moos++;
+        }
+      })
+      console.log("Moos: " + moos)
+
+      this.setState({
+        records: records.concat(
+          [
+            {
+              guess: this.state.input,
+              horns: horns,
+              hornIndeces: hornIndeces,
+              cows: moos
+            }
+          ]
+        )
+      }, () => {
+        console.log('Stored this data in records:')
+        console.log(this.state.records[this.state.records.length -1])
+        console.log('Records updated:')
+        console.log(this.state.records)
+      })
+
+      console.log('Resetting input field...')
+      document.getElementById("guessInput").reset()
     } else {
         alert("Please input a " + this.state.digits +"-digit number" )
       }
@@ -135,9 +143,10 @@ class Console extends Component {
         <h2>Numerical Bulls and Cows:</h2>
         <button onClick={this.newGame}>New Game</button>
         <p>I'm thinking of a {this.state.digits}-digit number...</p>
-          <form>
+          <form id="guessInput">
             <label>Guess:</label>
             <input
+
               value = {this.state.guess}
               onChange={this.inputGuess}
               type="number"
--- src/Components/Numerical/Results.js
+++ src/Components/Numerical/Results.js
@@ -6,7 +6,7 @@ class Guesses extends Component {
       <div>
         <h3>Guesses:</h3>
           <ol>
-            {this.props.records.map((record, index) => <li key={index}>{record.guess}: {record.bulls}B {record.cows}C</li>)}
+            {this.props.records.map((record, index) => <li key={index}>{record.guess}: {record.horns} Horns, {record.moos} Moos</li>)}
           </ol>
       </div>
     );
